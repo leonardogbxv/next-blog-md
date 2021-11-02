92c6881074a8e57cb01faa9b7497222c86a63ec0
Leonardo Gomes Batista <leonardog.bat@gmail.com>
2021-11-02 14:34:28 -0300

    Add admin login page

---
+++ pages/admin/login.js
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import styles from '../../styles/Form.module.scss'
+
+export default function Login() {
+  return (
+    <div className="container">
+      <section>
+        <h1>Administration Login</h1>
+        <form action="POST" className={styles.form}>
+          <div>
+            <label htmlFor="login">Login</label>
+            <input type="text" name="login" required />
+          </div>
+
+          <div>
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" required />
+          </div>
+
+          <button type="submit" class="btn-default">Sign In</button>
+          <Link href="/">
+            <button className="btn-default">Cancel</button>
+          </Link>
+        </form>
+
+      </section>
+    </div>
+  )
+}
