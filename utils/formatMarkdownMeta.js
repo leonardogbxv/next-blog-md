f52d43aa4646e435e2a55bf932c4543396766b1f
Leonardo Gomes Batista <leonardog.bat@gmail.com>
2021-11-11 21:01:36 -0300

    Add function to format markdown metadata

---
+++ utils/formatMarkdownMeta.js
@@ -0,0 +1,14 @@
+export function formatMarkdownMeta(markdown) {
+  const meta = `---\n\
+title: '${markdown.title}'\n\
+category: '${markdown.category}'\n\
+date: '${markdown.date}'\n\
+description: '${markdown.description}'\n\
+cover_image: '${markdown.image.fakePath}'\n\
+---\n\
+`
+
+  const formattedMD = meta + '\n' + markdown.markdown
+
+  return formattedMD
+}
