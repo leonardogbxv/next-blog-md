b84bd2116120d8697e0436ee6a647ae1f0199975
Leonardo Gomes Batista <leonardog.bat@gmail.com>
2022-01-24 18:00:30 -0300

    Add delete and edit icons on Posts component

--- components/Posts.js
+++ components/Posts.js
@@ -1,7 +1,12 @@
 import styles from '../styles/Posts.module.scss'
 import Link from 'next/link'
+import Icon from '@mui/material/Icon'
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
 export default function Posts({ posts }) {
+  const deletePost = () => {/* todo */}
+
   return (
     <main className={styles.content}>
       { posts.map((post, index) => (
@@ -15,9 +20,31 @@ export default function Posts({ posts }) {
               />
             </figure>
           </Link>
-          <h1>{post.meta.title}</h1>
-          <p className={styles.description}>{post.meta.description}</p>
-          <p className={styles.date}>{post.meta.date}</p>
+
+          <section>
+            <div>
+              <h1>{post.meta.title}</h1>
+              <p className={styles.description}>{post.meta.description}</p>
+              <p className={styles.date}>{post.meta.date}</p>
+            </div>
+
+            <div>
+              <div className={styles["edit-icon"]}>
+                <Icon color="inherit">
+                  <EditIcon />
+                </Icon>
+              </div>
+
+              <div 
+                className={styles["delete-icon"]}
+                onClick={() => deletePost()}
+              >
+                <Icon color="inherit">
+                  <DeleteIcon />
+                </Icon>
+              </div>
+            </div>
+          </section>
           <hr />
         </article>
       ))}
